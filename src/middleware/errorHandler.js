821964eac73a332ff0403958fb2f0bff9a36aadc
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-07-12 22:38:17 +0300

    add middleware

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,8 @@
+export const errorHandler = (err, req, res, next) => {
+  console.log('errorMiddleware', err);
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res
+    .status(500)
+    .json({ message: isProd ? 'Please, try again later' : err.message });
+};
