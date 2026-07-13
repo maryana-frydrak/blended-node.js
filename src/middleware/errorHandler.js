368182027b3c9e507865579667b20fe8bb8d3de1
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-07-13 15:53:30 +0300

    add routes

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,15 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
   console.log('errorMiddleware', err);
+
   const isProd = process.env.NODE_ENV === 'production';
+
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  }
 
   res
     .status(500)
