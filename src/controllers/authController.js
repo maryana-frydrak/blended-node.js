f18f0bbb584408529127ece5721e2efea70cf823
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-07-19 23:21:23 +0300

    login User

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
 import { createSession, setSessionCookies } from '../services/auth.js';
+import { Session } from '../models/session.js';
 
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -14,11 +15,35 @@ export const registerUser = async (req, res) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const user = await User.create(name, email, hashedPassword);
+  const user = await User.create({ name, email, password: hashedPassword });
 
-  const session = createSession(user._id);
+  const session = await createSession(user._id);
 
   setSessionCookies(res, session);
 
   res.status(201).json(user);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const session = await createSession(user._id);
+
+  setSessionCookies(res, session);
+
+  res.status(200).json(user);
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,14 @@
 import { celebrate } from 'celebrate';
 import { Router } from 'express';
-import { registerUserSchema } from '../validations/authValidation.js';
-import { registerUser } from '../controllers/authController.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validations/authValidation.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 
 export default router;
